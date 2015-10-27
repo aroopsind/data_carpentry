8772f7587b43de847e4c8c4734ac8cdcd8ff4d67
Anand Roopsind <aroopsind@gmail.com>
2015-10-27 12:28:37 -0400

    modified to reflect there are several data files on magical elves to be read into file

--- r_code/houseelf-analysis.R
+++ r_code/houseelf-analysis.R
@@ -1,4 +1,4 @@
 #Analyses of house-elves with Dr. Granger
 
 #import data: reads in data collected on magical house elves (doby death was most upsetting )
-house_elves <- read.csv("data/houseelf_earlength_dna_data.csv",header=TRUE)
+house_elves_1 <- read.csv("data/houseelf_earlength_dna_data_1.csv",header=TRUE) 
