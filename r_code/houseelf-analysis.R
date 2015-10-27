f1ef23c1fde05052a92c87f6798be102d21796ab
Anand Roopsind <Anand Roopsind>
2015-10-27 12:04:16 -0400

    #reads in house elve data

---
+++ r_code/houseelf-analysis.R
@@ -0,0 +1,4 @@
+#Analyses of house-elves with Dr. Granger
+
+#import data: reads in data collected on magical house elves (doby death was most upsetting )
+house_elves <- read.csv("data/houseelf_earlength_dna_data.csv",header=TRUE)
