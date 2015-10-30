cab2e6ea9f218ad930a80f08b6b8159cf1f99209
Ethan White <ethan@weecology.org>
2015-10-30 13:05:09 -0400

    Add size class function

--- r_code/houseelf-analysis.R
+++ r_code/houseelf-analysis.R
@@ -2,3 +2,9 @@
 
 #import data: reads in data collected on magical house elves (doby death was most upsetting )
 house_elves_1 <- read.csv("data/houseelf_earlength_dna_data_1.csv",header=TRUE) 
+
+get_size_class <- function(seq){
+   #Calculate the GC-content for one or more sequences
+   ear_lengths <- ifelse(seq > 10, "large", "small")
+   return(ear_lengths)
+}
