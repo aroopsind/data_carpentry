f238cb29124bdd418ef65d77e9533df9649ad554
Anand Roopsind <aroopsind@gmail.com>
2015-11-02 10:46:40 -0500

    #creates a dataframe with dna GC content, ear classification and elve ID

--- r_code/houseelf-analysis.R
+++ r_code/houseelf-analysis.R
@@ -17,17 +17,38 @@ if (sum(str_count(data, 'G')) | sum(str_count(data,'C')) > 0) {
       Gs <- str_count(gsdata, 'g')
       Cs <- str_count(gsdata, 'c')
       gc_content <- (Gs + Cs) / str_length(data) * 100
+      return(gc_content)
       
 }
 
 #print out gc content of dna sequences observed
-print(read_gc_content(house_elves_1$dnaseq))
-=======
-house_elves_1 <- read.csv("data/houseelf_earlength_dna_data_1.csv",header=TRUE) 
+dna_content <- read_gc_content(house_elves_1$dnaseq)
+print(dna_content)
+
+======= #CODE ADDED BY DOC GRANGER
+# house_elves_1 <- read.csv("data/houseelf_earlength_dna_data_1.csv",header=TRUE) #data already read into r
 
 get_size_class <- function(seq){
-   #Calculate the GC-content for one or more sequences
+   #classifies ear as small when ear length < 10, large otherwise
    ear_lengths <- ifelse(seq > 10, "large", "small")
    return(ear_lengths)
 }
+
+ear_lengths <- get_size_class(house_elves_1$earlength)
+print(ear_lengths)
+
 >>>>>>> cab2e6ea9f218ad930a80f08b6b8159cf1f99209
+======= # END CODE ADDED BY DOC
+
+#creates dataframe with information about individual ID, ear length class and gc content
+ID <- house_elves_1$id #storing elve ID as vector
+print(ID)
+
+#combine ID with outputs from dna sequence and ear classification functions
+house_elves_magic <- data.frame(ID,dna_content,ear_lengths)
+print(house_elves_magic)
+
+#write as csv file
+write.csv(house_elves_magic, "outputs/granger_elve_analysis.csv")
+
+
